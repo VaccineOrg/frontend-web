66c62a89df7a8db9f1fe43a68b634f822ee0676e
luizggritte <gustavogritte@hotmail.com>
2021-05-21 20:49:39 -0300

    :lipstick: Adicionado componente de Cabecalho

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
+import Header from '../components/Header'
+
 import theme from '../styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <Header />
       <Component {...pageProps} />
     </ChakraProvider>
   )

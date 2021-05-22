72aeaa16dcc3838b4b759bae70197bf1fddb2b82
luizggritte <gustavogritte@hotmail.com>
2021-05-21 21:16:33 -0300

    :truck: Renomeado o servico de comunicacao com o backend

---
+++ src/services/ApiService.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const httpClient = axios.create({
+  baseURL: "http://localhost:8081"
+})
+
+class ApiService {
+  apiUrl: string;
+
+  constructor(apiUrl: string) {
+    this.apiUrl = apiUrl;
+  }
+
+  get(url: string) {
+    return httpClient.get(`${this.apiUrl}${url}`);
+  }
+
+  post(url: string, object: object) {
+    return httpClient.post(`${this.apiUrl}${url}`, object);
+  }
+
+  put(url: string, object: object) {
+    return httpClient.put(`${this.apiUrl}${url}`, object);
+  }
+
+  delete(url: string) {
+    return httpClient.delete(`${this.apiUrl}${url}`);
+  }
+}
+
+export default ApiService

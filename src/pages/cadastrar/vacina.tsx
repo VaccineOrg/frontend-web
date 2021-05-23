20c01671e92fee852279a18873b9de774b7d67dc
luizggritte <gustavogritte@hotmail.com>
2021-05-23 18:51:45 -0300

    :sparkles: Criadas as funcionalidades de Cadastrar, Editar e Excluir vacinas

--- src/pages/cadastrar/vacina.tsx
+++ src/pages/cadastrar/vacina.tsx
@@ -1,4 +1,9 @@
-import React from "react"
+import React, { useRef, useState } from "react"
+
+import * as yup from "yup"
+import { useForm } from "react-hook-form"
+import { useRouter } from "next/dist/client/router"
+import { yupResolver } from '@hookform/resolvers/yup'
 import { GetServerSideProps } from "next"
 import { MdDelete, MdModeEdit } from "react-icons/md"
 import {
@@ -13,11 +18,13 @@ import {
   Button,
   Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
   HStack,
   IconButton,
   Input,
+  Stack,
   Table,
   TableCaption,
   Tbody,
@@ -28,6 +35,12 @@ import {
   useDisclosure
 } from "@chakra-ui/react"
 
+import {
+  showErrorMessage,
+  showSuccessMessage,
+  ToastComponent
+} from "../../components/Toast"
+
 import VaccineService from "../../services/VaccineService"
 
 type Vaccine = {
@@ -37,43 +50,145 @@ type Vaccine = {
   name: string,
 }
 
-type VacinaProps = {
-  vaccines: Vaccine[]
+interface VacinaProps {
+  vaccines: Vaccine[],
 }
+
+type VaccineData = Omit<Vaccine, "id" | "ableToDelete">
+
+const service = new VaccineService("10")
 
 function Vacina({ vaccines }: VacinaProps) {
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const cancelRef = React.useRef(null)
+  const cancelRef = useRef(null)
+  const router = useRouter()
+
+  const [vaccineId, setVaccineId] = useState<number | null>(null)
+
+  const schema = yup.object().shape({
+    description: yup.string(),
+    name: yup.string().required("Nome da vacina é obrigatório"),
+  })
+
+  const {
+    handleSubmit,
+    setValue,
+    register,
+    formState: { errors },
+    reset
+  } = useForm<VaccineData>({
+    resolver: yupResolver(schema)
+  })
+
+  const handleGetAll = async () => {
+    router.replace(router.asPath)
+  }
+
+  const handleEdit = (vaccine: Vaccine) => {
+    setValue("name", vaccine.name)
+    setValue("description", vaccine.description)
+
+    setVaccineId(vaccine.id)
+  }
+
+  const onReset = () => {
+    reset()
+
+    setVaccineId(null)
+  }
+
+  const onSubmit = (data: VaccineData) => {
+    if (vaccineId) {
+      service.updateVaccine(vaccineId, data)
+        .then(() => {
+          onReset()
+          handleGetAll()
+          showSuccessMessage("Vacina editada com sucesso")
+        })
+        .catch(err => showErrorMessage(err.message))
+    }
+    else {
+      service.createVaccine(data)
+        .then(() => {
+          onReset()
+          handleGetAll()
+          showSuccessMessage("Vacina criada com sucesso")
+        })
+        .catch(err => showErrorMessage(err.message))
+    }
+  }
+
+  const handleDelete = (vaccine: Vaccine) => {
+    onOpen()
+
+    setVaccineId(vaccine.id)
+  }
+
+  const refuseDelete = () => {
+    onClose()
+
+    setVaccineId(null)
+  }
+
+  const confirmDelete = () => {
+    onClose()
+
+    if (vaccineId) {
+      service.deleteVaccine(vaccineId)
+        .then(() => {
+          handleGetAll()
+          showSuccessMessage("Vacina deletada com sucesso")
+        })
+        .catch(err => showErrorMessage(err.message))
+    }
+  }
 
   return (
     <Flex w="100%" maxW="1160" mx="auto" direction="column">
       <Heading mt="12">Cadastrar Vacina</Heading>
       <Box mt={12}>
-        <form>
-          <FormControl id="nome" isRequired>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl id="name" isInvalid={!!errors && !!errors["name"]}>
             <FormLabel>Nome da Vacina</FormLabel>
-            <Input placeholder="Nome da vacina - Mês/Ano" type="text" />
+            <Input
+              placeholder="Nome da vacina - Mês/Ano"
+              {...register("name")}
+            />
+            <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
           </FormControl>
-          <FormControl id="dica" mt={4} isRequired>
-            <FormLabel>Dica</FormLabel>
-            <Input placeholder="Vacina contra ..." type="text" />
-          </FormControl>
-          <Button
+          <FormControl
             mt={4}
-            isLoading={false}
-            loadingText="Salvando"
-            variant="outline"
+            id="description"
+            isInvalid={!!errors && !!errors["description"]}
           >
-            Salvar
-          </Button>
+            <FormLabel>Dica</FormLabel>
+            <Input
+              placeholder="Vacina contra ..."
+              {...register("description")}
+            />
+            <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
+          </FormControl>
+          <Stack direction="row-reverse" spacing={4} mt={4}>
+            <Button
+              type="submit"
+              isLoading={false}
+              loadingText="Salvando"
+            >
+              Salvar
+            </Button>
+            <Button
+              type="reset"
+              onClick={onReset}
+            >
+              Limpar
+            </Button>
+          </Stack>
         </form>
       </Box>
       <Table mt={12}>
         {
           vaccines.length == 0 &&
-          <TableCaption>
-            Nenhuma vacina foi cadastrada
-          </TableCaption>
+          <TableCaption>Nenhuma vacina foi cadastrada</TableCaption>
         }
         <Thead>
           <Tr bg="lightgray">
@@ -91,16 +206,16 @@ function Vacina({ vaccines }: VacinaProps) {
                 <Td>
                   <HStack spacing={2}>
                     <IconButton
-                      variant="outline"
-                      aria-label="Editar informações da vacina"
                       icon={<MdModeEdit />}
+                      isDisabled={!vaccine.ableToDelete}
+                      onClick={() => handleEdit(vaccine)}
+                      aria-label="Editar informações da vacina"
                     />
                     <IconButton
-                      onClick={onOpen}
-                      isDisabled={!vaccine.ableToDelete}
-                      variant="outline"
-                      aria-label="Excluir vacina"
                       icon={<MdDelete />}
+                      isDisabled={!vaccine.ableToDelete}
+                      onClick={() => handleDelete(vaccine)}
+                      aria-label="Excluir vacina"
                     />
                   </HStack>
                 </Td>
@@ -110,39 +225,36 @@ function Vacina({ vaccines }: VacinaProps) {
         </Tbody>
       </Table>
       <AlertDialog
+        isCentered
+        isOpen={isOpen}
+        onClose={onClose}
         motionPreset="slideInBottom"
         leastDestructiveRef={cancelRef}
-        onClose={onClose}
-        isOpen={isOpen}
-        isCentered
       >
         <AlertDialogOverlay />
         <AlertDialogContent>
-          <AlertDialogHeader>Excluir ?</AlertDialogHeader>
+          <AlertDialogHeader>
+            Excluir ?
+          </AlertDialogHeader>
           <AlertDialogCloseButton />
           <AlertDialogBody>
             Tem certeza que gostaria de excluir a vacina ?
           </AlertDialogBody>
           <AlertDialogFooter>
-            <Button ref={cancelRef} onClick={onClose}>Cancelar</Button>
-            <Button ml={3} onClick={onClose}>Confirmar</Button>
+            <Button ref={cancelRef} onClick={refuseDelete}>Cancelar</Button>
+            <Button ml={3} onClick={confirmDelete}>Confirmar</Button>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
+      <ToastComponent />
     </Flex>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (_context) => {
-  const USER_PROFILE = "10"
-
-  const service = new VaccineService(USER_PROFILE)
-
   const { data } = await service.getAllVaccines()
 
-  return {
-    props: { vaccines: data }
-  }
+  return { props: { vaccines: data } }
 }
 
 export default Vacina

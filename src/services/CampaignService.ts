a27a238a214cf5f499b067cd2502b3c199ff7973
luizggritte <gustavogritte@hotmail.com>
2021-05-29 11:25:28 -0300

    :sparkles: Criado o servico referente a requisicoes sobre campanhas

--- src/services/CampaignService.ts
+++ src/services/CampaignService.ts
@@ -3,25 +3,44 @@ import { AxiosResponse } from "axios";
 import ApiService from "./ApiService";
 
 type Campaign = {
-	userId: number,
-	campaignId: number,
-	vaccineId: number,
-	status: string,
-	campaignName: string,
-	vaccineName: string
+    dateBegin: Date,
+    dateEnd: Date,
+    id: number,
+    idVaccine: number,
+    name: string,
+    numberVaccines: number,
+    status: string
 }
 
 type Campaigns = {
     userCampaigns: Campaign[]
 }
 
+type CampaignData = Omit<Campaign, "id">
+
 class CampaignService extends ApiService {
     constructor(userProfile?: string) {
         super("/v1/vaccination", userProfile);
     }
 
+    getAllCampaigns(): Promise<AxiosResponse<Campaigns>> {
+        return this.get("/")
+    }
+
     getAllCampaignsByUser(name: string): Promise<AxiosResponse<Campaigns>> {
         return this.get(`/${name}/campaigns`)
+    }
+
+    createCampaign(campaign: CampaignData): Promise<AxiosResponse<Campaign>> {
+        return this.post("/", campaign)
+    }
+
+    updateCampaign(id: number, campaign: CampaignData): Promise<AxiosResponse<Campaign>> {
+        return this.put(`/${id}`, campaign)
+    }
+
+    deleteCampaign(id: number) {
+        return this.delete(`/${id}`)
     }
 }
 

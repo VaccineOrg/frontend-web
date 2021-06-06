ab2546465aa05778a001aec9b327634ade6cd11d
luizggritte <gustavogritte@hotmail.com>
2021-06-06 14:25:53 -0300

    :sparkles: Criado layout padrao para pagina de registro

---
+++ src/pages/registro.tsx
@@ -0,0 +1,18 @@
+import { Box, Flex, Heading } from "@chakra-ui/layout"
+
+function Registro() {
+  return (
+    <Flex
+      w="100%"
+      maxW="1160"
+      mx="auto"
+      direction="column"
+    >
+      <Heading mt="12">Criar uma conta</Heading>
+      <Box mt={12}>
+      </Box>
+    </Flex>
+  )
+}
+
+export default Registro

bc3cef2cffa316e62dc396eef0c399df2a3ff911
luizggritte <gustavogritte@hotmail.com>
2021-06-06 18:57:05 -0300

    :sparkles: Criado serviço de usuário

---
+++ src/services/UserService.ts
@@ -0,0 +1,25 @@
+import { AxiosResponse } from "axios";
+
+import ApiService from "./ApiService";
+
+import { User, UserData } from "../types/User";
+
+class UserService extends ApiService {
+    constructor() {
+        super("/v1/user");
+    }
+
+    createUser(user: UserData): Promise<AxiosResponse<User>> {
+        return this.post("/register", user)
+    }
+
+    updateUser(id: number, user: UserData): Promise<AxiosResponse<User>> {
+        return this.put(`/${id}`, user)
+    }
+
+    deleteUser(id: number) {
+        return this.delete(`/${id}`)
+    }
+}
+
+export default UserService
